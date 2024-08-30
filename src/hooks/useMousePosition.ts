555bb817cd36c74ba2dacecb496261d5e2fccc3c
yash-devop <kamble1234meena@gmail.com>
2024-08-30 17:03:24 +0530

    add: useMousePosition on a specific div

---
+++ src/hooks/useMousePosition.ts
@@ -0,0 +1,36 @@
+import { RefObject, useEffect, useState } from "react"
+
+
+
+const useMousePosition = (containerRef: RefObject<HTMLDivElement>, maxXOffset: number, maxYOffset: number) => {
+  const [mousePosition, setMousePosition] = useState<{ x: number; y: number } | null>({
+    x: 0,
+    y: 0
+  });
+
+  useEffect(() => {
+    const updateMousePosition = (e: MouseEvent) => {
+      const rect = containerRef.current?.getBoundingClientRect();
+
+      if (rect) {
+        const maxX = rect.width + maxXOffset;
+        const maxY = rect.height + maxYOffset;
+
+        setMousePosition({
+          x: Math.max(30, Math.min(e.clientX - rect.left + 40, maxX)),
+          y: Math.max(30, Math.min(e.clientY - rect.top + 40, maxY))
+        });
+      }
+    };
+
+    containerRef.current?.addEventListener("mousemove", updateMousePosition);
+
+    return () => {
+      containerRef.current?.removeEventListener("mousemove", updateMousePosition);
+    };
+  }, [containerRef, maxXOffset, maxYOffset]);
+
+  return mousePosition;
+};
+
+export default useMousePosition;
