cfe4ec6ddaeba8fa2861eed3eed760ab9837694a
yash-devop <kamble1234meena@gmail.com>
2024-08-30 20:07:59 +0530

    add: theme switch

---
+++ src/components/ThemeSwitch.tsx
@@ -0,0 +1,41 @@
+
+import { cn } from "@/lib/utils";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react"
+
+export default function ThemeSwitch() {
+    const [mounted, setMounted] = useState(false)
+    const { theme, setTheme } = useTheme();
+
+    // useEffect only runs on the client, so now we can safely show the UI
+    useEffect(() => {
+        setMounted(true)
+    }, [])
+
+    if (!mounted) {
+        return null
+    }
+
+    const toggleTheme = () => {
+        setTheme(theme === "dark" ? "light" : "dark");
+    };
+
+    return (
+        <>
+            <div onClick={toggleTheme}
+            className={cn(
+                "rounded-full p-1.5 cursor-pointer",
+                theme === "light" ? "hover:bg-gray-100" : "hover:bg-white/10"
+            )}>
+                {
+                    theme === "light" ? (
+                        <Moon />
+                    ) : (
+                        <Sun size={18} />
+                    )
+                }
+            </div>
+        </>
+    )
+}
