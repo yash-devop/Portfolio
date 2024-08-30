d2f7a9f6df50f4747be8309f176811ee20b017f0
yash-devop <kamble1234meena@gmail.com>
2024-08-30 20:08:19 +0530

    add: techstack

---
+++ src/app/stack/page.tsx
@@ -0,0 +1,17 @@
+import Heading from "@/components/ui/Heading";
+
+export default function Page() {
+    return (
+        <>
+            <div>
+                <div className="tracking-tighter pt-20">
+                    <Heading className="w-full max-w-xl">Tech Stack</Heading>
+                    <h1 className="text-[32px] "></h1>
+                    <p className="text-[16px] py-5 leading-relaxed text-[#161616]">" Crafting designs and building products that should be able to solve<br />
+                        some of your problems and help make your life
+                        uncomplicated + aesthetically pleasing "</p>
+                </div>
+            </div>
+        </>
+    )
+}
