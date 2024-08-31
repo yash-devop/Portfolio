3d5d856808e9d5d53fda127e20ce9cf2246c27bb
yash-devop <kamble1234meena@gmail.com>
2024-08-31 20:57:57 +0530

    add: attribute class

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider>
+        <ThemeProvider attribute="class">
           <Navbar />
           <Container>
             {children}
