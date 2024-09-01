72ecba7ad7ace163a85b5aeb0f2b168e34f9dfcd
yash-devop <kamble1234meena@gmail.com>
2024-09-01 19:38:46 +0530

    chore: rm scrap details

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -3,33 +3,10 @@ import { Timeline } from "./(component)/Timeline";
 
 const ResumeDetails = [
     {
-        duration: "Mar. 2023 · Aug. 2023",
-        title : "Feature head and Full-Stack developer",
-        company: "Hypefury",
-        description: "As a Project Lead, my responsibility was to oversee and maintain all the codebase of one of Hypefury's core offerings. Typescript, Rust, Python were used in the infrastructure. We utilized NextJS with the “T-3 stack” - ensuring developer productivity as well as a bug-free typesafe codebase."
-    },
-    {
-        duration: "Mar. 2023 · Aug. 2023",
-        title : "Feature head and Full-Stack developer",
-        company: "Hypefury",
-        description: "As a Project Lead, my responsibility was to oversee and maintain all the codebase of one of Hypefury's core offerings. Typescript, Rust, Python were used in the infrastructure. We utilized NextJS with the “T-3 stack” - ensuring developer productivity as well as a bug-free typesafe codebase."
-    },
-    {
-        duration: "Mar. 2023 · Aug. 2023",
-        title : "Feature head and Full-Stack developer",
-        company: "Hypefury",
-        description: "As a Project Lead, my responsibility was to oversee and maintain all the codebase of one of Hypefury's core offerings. Typescript, Rust, Python were used in the infrastructure. We utilized NextJS with the “T-3 stack” - ensuring developer productivity as well as a bug-free typesafe codebase."
-    },
-    {
-        duration: "Mar. 2023 · Aug. 2023",
-        title : "Feature head and Full-Stack developer",
-        company: "Hypefury",
-        description: "As a Project Lead, my responsibility was to oversee and maintain all the codebase of one of Hypefury's core offerings. Typescript, Rust, Python were used in the infrastructure. We utilized NextJS with the “T-3 stack” - ensuring developer productivity as well as a bug-free typesafe codebase."
-    },
-    {
-        duration: "Mar. 2023 · Aug. 2023",
-        title : "Feature head and Full-Stack developer",
-        company: "Hypefury",
+        duration: "Dec. 2023 · Apr. 2024",
+        logo: "/assets/icons/iitb_logo.png",
+        title : "Tech lead and Full Stack Developer",
+        company: "Indian Institute of Technology, Bombay · Internship",
         description: "As a Project Lead, my responsibility was to oversee and maintain all the codebase of one of Hypefury's core offerings. Typescript, Rust, Python were used in the infrastructure. We utilized NextJS with the “T-3 stack” - ensuring developer productivity as well as a bug-free typesafe codebase."
     },
 
@@ -39,10 +16,10 @@ export default function Resume(){
         <>
             <div className="pt-20">
                 <Heading>Resume</Heading>
-                <div className="px-6 py-16">
+                <div className="px-8 py-16">
                     {
-                        ResumeDetails.map(({company,description,duration,title},idx)=>(
-                            <Timeline length={ResumeDetails.length} idx={idx} company={company} description={description} duration={duration} title={title} />
+                        ResumeDetails.map(({company,description,duration,title,logo},idx)=>(
+                            <Timeline length={ResumeDetails.length} idx={idx} company={company} description={description} duration={duration} title={title} logo={logo}/>
                         ))
                     }
                 </div>
