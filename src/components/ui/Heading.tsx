8a5cb06e79fc6156de27e2222800ed6067217e9e
yash-devop <kamble1234meena@gmail.com>
2024-09-01 12:27:29 +0530

    chore: change tailwind color class

--- src/components/ui/Heading.tsx
+++ src/components/ui/Heading.tsx
@@ -3,7 +3,7 @@ import { cn } from "@/lib/utils"
 export default function Heading(props:React.HTMLProps<HTMLHeadingElement>) {
     return (
         <>
-            <h1 {...props} className={cn("text-[32px] tracking-tighter leading-tight text-black dark:text-white", props.className)}>{props.children}</h1>
+            <h1 {...props} className={cn("text-[32px] tracking-tighter leading-tight text-primary dark:text-white", props.className)}>{props.children}</h1>
         </>
     )
 }
