56a545a148a96c62f7899897d50338480c0565b2
yash-devop <kamble1234meena@gmail.com>
2024-09-01 19:50:33 +0530

    chore: move the array to constants folder

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,16 +1,7 @@
 import Heading from "@/components/ui/Heading";
 import { Timeline } from "./(component)/Timeline";
+import { ResumeDetails } from "@/constants";
 
-const ResumeDetails = [
-    {
-        duration: "Dec. 2023 · Apr. 2024",
-        logo: "/assets/icons/iitb_logo.png",
-        title : "Tech lead and Full Stack Developer",
-        company: "Indian Institute of Technology, Bombay · Internship",
-        description: "As a Project Lead, my responsibility was to oversee and maintain all the codebase of one of Hypefury's core offerings. Typescript, Rust, Python were used in the infrastructure. We utilized NextJS with the “T-3 stack” - ensuring developer productivity as well as a bug-free typesafe codebase."
-    },
-
-]
 export default function Resume(){
     return (
         <>
---
+++ src/constants.ts
@@ -0,0 +1,74 @@
+export const Skills = [
+    {
+       name: "Next JS",
+       icon: "/assets/icons/nextjs_ico.svg"
+    },
+    {
+       name: "React JS",
+       icon: "/assets/icons/react_ico.svg"
+    },
+    {
+       name: "Typescript",
+       icon: "/assets/icons/typescript_ico.svg"
+    },
+    {
+       name: "Prisma ORM",
+       icon: "/assets/icons/prisma_ico.svg"
+    },
+    {
+       name: "Javascript",
+       icon: "/assets/icons/javascript_ico.svg"
+    },
+    {
+       name: "Tailwind CSS",
+       icon: "/assets/icons/tailwind_ico.svg"
+    },
+    {
+       name: "Node JS",
+       icon: "/assets/icons/node_ico.svg"
+    },
+    {
+       name: "React Query",
+       icon: "/assets/icons/reactquery_ico.svg"
+    },
+    {
+       name: "Redis",
+       icon: "/assets/icons/redis_ico.svg"
+    },
+    {
+       name: "Redux Toolkit",
+       icon: "/assets/icons/redux_ico.svg"
+    },
+    {
+       name: "Postgres SQL",
+       icon: "/assets/icons/postgres_ico.svg"
+    },
+    {
+       name: "Mongo DB",
+       icon: "/assets/icons/mongo_ico.svg"
+    },
+    {
+       name: "Docker",
+       icon: "/assets/icons/docker_ico.svg"
+    },
+    {
+       name: "Figma",
+       icon: "/assets/icons/figma_ico.svg"
+    },
+    {
+       name: "AWS",
+       icon: "/assets/icons/aws_ico.svg"
+    },
+];
+
+
+export const ResumeDetails = [
+    {
+        duration: "Dec. 2023 · Apr. 2024",
+        logo: "/assets/icons/iitb_logo.png",
+        title : "Tech lead and Full Stack Developer",
+        company: "Indian Institute of Technology, Bombay · Internship",
+        description: "As a Project Lead, my responsibility was to oversee and maintain all the codebase of one of Hypefury's core offerings. Typescript, Rust, Python were used in the infrastructure. We utilized NextJS with the “T-3 stack” - ensuring developer productivity as well as a bug-free typesafe codebase."
+    },
+
+]
