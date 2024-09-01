14f446ce20ab5c154065bf2dec58cef989ad072b
yash-devop <kamble1234meena@gmail.com>
2024-09-01 13:37:49 +0530

    chore: hover text white

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -11,7 +11,7 @@ export default function SkillCard({
         <>
             <div className="w-fit bg-secondary/10 py-2 px-4 rounded-lg border flex items-center gap-2 border-secondary/5 group-hover:opacity-25 hover:!opacity-100 cursor-pointer transition-all">
                 <Image alt="next_svg" width={25} height={25} src={icon} className="object-contain fill-blue-400" />
-                <span className="text-[16px] font-medium ">{name}</span>
+                <span className="text-[16px] font-medium  text-primary dark:text-secondary-dark dark:group-hover:text-white">{name}</span>
             </div>
         </>
     )
