916b052bf66c8b1bc3884c22203ec28530f73ccc
yash-devop <kamble1234meena@gmail.com>
2024-09-04 10:22:43 +0530

    chore: padding responsive

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -9,7 +9,7 @@ export default function SkillCard({
 }){
     return (
         <>
-            <div className="w-fit bg-secondary/10 py-2 px-4 rounded-lg border flex items-center gap-2 border-secondary/5 group-hover:opacity-25 hover:!opacity-100 cursor-pointer transition-all">
+            <div className="w-fit bg-secondary/10 px-3 py-2 md:py-2 md:px-4 rounded-lg border flex items-center gap-2 border-secondary/5 group-hover:opacity-25 hover:!opacity-100 cursor-pointer transition-all">
                 <Image alt="next_svg" width={25} height={25} src={icon} className="object-contain fill-blue-400" />
                 <span className="text-[16px] font-medium  text-primary dark:text-secondary-dark dark:group-hover:text-white">{name}</span>
             </div>
