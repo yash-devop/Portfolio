be17c21da8158a138c13dc8ac4a5c24f517e1e93
yash-devop <kamble1234meena@gmail.com>
2024-09-07 18:57:06 +0530

    chore: useCallback and useKeyboardShortcut custom hook

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -1,13 +1,27 @@
-
+"use client"
+import { useKeyboardShortcut } from "@/hooks/useKeyboardShortcut";
 import { cn } from "@/lib/utils";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 import { flushSync } from "react-dom";
 
 export default function ThemeSwitch() {
     const [mounted, setMounted] = useState(false)
     const { theme, setTheme } = useTheme();
+
+    const toggleTheme = useCallback(() => {
+        document.startViewTransition(()=>{
+            flushSync(()=>{
+                setTheme(theme === "dark" ? "light" : "dark");
+            })
+        })
+    }, [theme, setTheme]);
+
+    useKeyboardShortcut({
+        key: "l",
+        onPressed: toggleTheme
+    })
 
     // useEffect only runs on the client, so now we can safely show the UI
     useEffect(() => {
@@ -18,23 +32,13 @@ export default function ThemeSwitch() {
         return null
     }
 
-    const toggleTheme = () => {
-        document.startViewTransition(()=>{
-            flushSync(()=>{
-
-                setTheme(theme === "dark" ? "light" : "dark");
-            })
-        })
-    };
-
-
     return (
         <>
             <div onClick={toggleTheme}
-            className={cn(
-                "rounded-full p-1.5 cursor-pointer",
-                theme === "light" ? "hover:bg-gray-100" : "hover:bg-white/10"
-            )}>
+                className={cn(
+                    "rounded-full p-1.5 cursor-pointer",
+                    theme === "light" ? "hover:bg-gray-100" : "hover:bg-white/10"
+                )}>
                 {
                     theme === "light" ? (
                         <Moon size={18}/>
